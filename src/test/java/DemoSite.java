984fa955612729f6639d4573c5acd17ee4603016
PAchiilleos <Shadowgrey4>
2017-08-08 11:52:35 +0100

    Second Commit

--- src/test/java/DemoSite.java
+++ src/test/java/DemoSite.java
@@ -5,49 +5,42 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
-import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.support.PageFactory;
 
 public class DemoSite {
 
 private WebDriver wd;
+private Homepage hp;
+private CreateUser cu;
+private Login l;
 
     @Before
     public void before() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--start-maximized");
+        PSE P = new PSE();
         wd = new ChromeDriver(options);
+        hp = PageFactory.initElements(wd,Homepage.class);
+        cu = PageFactory.initElements(wd,CreateUser.class);
+        l= PageFactory.initElements(wd,Login.class);
 
     }
 
 
     @Test
     public void demsit() {
-        PSE P = new PSE();
+
         wd.navigate().to("http://thedemosite.co.uk/index.php");
-        P.t(1);
-        wd.findElement(By.xpath("/html/body/div/center/table/tbody/tr[2]/td/div/center/table/tbody/tr/td[2]/p/small/a[3]")).click();
-        P.t(1);
-        wd.findElement(By.cssSelector("form center table tr:nth-child(1) > td:nth-child(2) input")).sendKeys("Pro2");
-        P.t(1);
-        wd.findElement(By.cssSelector("form center table tr:nth-child(2) td:nth-child(2) input[type=\"password\"]")).sendKeys("abcdefg");
-        P.t(1);
-        wd.findElement(By.xpath("/html/body/table/tbody/tr/td[1]/form/div/center/table/tbody/tr/td[1]/div/center/table/tbody/tr[3]/td[2]/p/input")).click();
-        P.t(1);
-        wd.findElement(By.xpath("/html/body/div/center/table/tbody/tr[2]/td/div/center/table/tbody/tr/td[2]/p/small/a[4]")).click();
-        P.t(1);
-        wd.findElement(By.cssSelector("form center table tr:nth-child(1) td:nth-child(2)  input")).sendKeys("Pro2");
-        P.t(1);
-        wd.findElement(By.cssSelector("form center table tr:nth-child(2) td:nth-child(2) input[type=\"password\"]")).sendKeys("abcdefg");
-        P.t(1);
-        wd.findElement(By.xpath("/html/body/table/tbody/tr/td[1]/form/div/center/table/tbody/tr/td[1]/table/tbody/tr[3]/td[2]/p/input")).click();
-        P.t(2);
+        hp.clickthis();
 
+        cu.enterusername("Pro3");
+        cu.enterpassword("abcdefg");
+        cu.click1();
+        cu.click2();
 
-
-
-
-
+        l.enterusername("Pro3");
+        l.enterpassword("abcdefg");
+        l.click1();
 
 
     }
@@ -55,5 +48,6 @@ private WebDriver wd;
     public void af(){
         wd.quit();
     }
+
 
 }
